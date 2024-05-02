faa7bdc87db0f70cdd0e1ad109e669516885b9dd
Karl Ericsson <lexicon@services.karlericsson.com>
2024-05-02 13:53:49 +0200

    Add database connection

---
+++ Data/GarageDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Garage.Models;
+
+namespace Garage.Data;
+
+public class GarageDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Vehicle> Vehicles { get; set; }
+    public DbSet<ParkingEvent> ParkingEvents { get; set; }
+    public DbSet<VehicleType> VehicleTypes { get; set; }
+
+
+    protected readonly IConfiguration Configuration;
+
+    public GarageDbContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        // connect to sqlite database
+        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+    }
+}
