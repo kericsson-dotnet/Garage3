e34647f101ceec067bc779f736b8af2ebcb3e7e4
Karl Ericsson <lexicon@services.karlericsson.com>
2024-05-03 13:31:17 +0200

    Validations for User and Vehicle models

--- Models/User.cs
+++ Models/User.cs
@@ -1,10 +1,16 @@
-﻿namespace Garage.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Garage.Models
 {
     public class User
     {
         public int UserId { get; set; }
+        [RegularExpression(@"^\d{6}-\d{4}$", ErrorMessage = "Personal number must be in the format YYMMDD-XXXX")]
+
         public string PersonalNumber { get; set; }
+        [MaxLength(35)]
         public string FirstName { get; set; }
+        [MaxLength(35)]
         public string LastName { get; set; }
         public int Age { get; set; }
         public List<Vehicle> Vehicles { get; set; }
