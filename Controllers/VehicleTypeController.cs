cc3dd3ed7860334335c47483db415a0c35d2859e
jenscodejens <jensb.code@gmail.com>
2024-05-03 13:46:27 +0200

    Lagt till VehicleType

---
+++ Controllers/VehicleTypeController.cs
@@ -0,0 +1,40 @@
+﻿using Garage.Data;
+using Garage.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Garage.Controllers
+{
+    public class VehicleTypeController : Controller
+    {
+        private readonly IRepository<VehicleType> _repository;
+
+        public VehicleTypeController(IRepository<VehicleType> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var vehicletypes = await _repository.GetAll();
+            return View(vehicletypes);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(VehicleType vehicletype)
+        {
+            if (ModelState.IsValid)
+            {
+                await _repository.Add(vehicletype);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vehicletype);
+        }
+
+        // Implement other actions (Edit, Details, Delete)...
+    }
+}
---
+++ Data/VehicleTypeRepository.cs
@@ -0,0 +1,42 @@
+﻿using Garage.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Garage.Data;
+public class VehicleTypeRepository : IRepository<VehicleType>
+{
+    private readonly GarageDbContext _context;
+
+    public VehicleTypeRepository(GarageDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<VehicleType>> GetAll()
+    {
+        return await _context.VehicleTypes.ToListAsync();
+    }
+
+    public async Task<VehicleType> Get(int id)
+    {
+        return await _context.VehicleTypes.FindAsync(id);
+    }
+
+    public async Task Add(VehicleType vehicletype)
+    {
+        _context.VehicleTypes.Add(vehicletype);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task Update(VehicleType vehicletype)
+    {
+        _context.Entry(vehicletype).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task Delete(int id)
+    {
+        var vehicletype = await _context.VehicleTypes.FindAsync(id);
+        _context.VehicleTypes.Remove(vehicletype);
+        await _context.SaveChangesAsync();
+    }
+}
