b1161495a3049bcee844e552e7cc0069b0ea97e5
Karl Ericsson <lexicon@services.karlericsson.com>
2024-05-05 13:57:29 +0200

    More basic seed + tweaks

--- Program.cs
+++ Program.cs
@@ -16,9 +16,6 @@ builder.Services.AddScoped<IRepository<Vehicle>, VehicleRepository>();
 builder.Services.AddScoped<IRepository<VehicleType>, VehicleTypeRepository>();
 builder.Services.AddScoped<IRepository<ParkingEvent>, ParkingEventRepository>();
 
-builder.Services.AddDbContext<GarageDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.")));
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -40,7 +37,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Vehicles}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Services.GetService<IHostApplicationLifetime>().ApplicationStarted.Register(async () =>
 {
@@ -62,18 +59,50 @@ app.Services.GetService<IHostApplicationLifetime>().ApplicationStarted.Register(
             };
             await userRepository.Add(newUser);
         }
+        
+        // Basic seed for VehicleTypes
+        var vehicleTypeRepository = scope.ServiceProvider.GetRequiredService<IRepository<VehicleType>>();
+        var vehicleTypes = await vehicleTypeRepository.GetAll();
+        if (!vehicleTypes.Any())
+        {
+            var newVehicleType = new VehicleType
+            {
+                TypeName = "Car",
+                SlotsOccupied = 1
+            };
+            await vehicleTypeRepository.Add(newVehicleType);
+        }
+
+        // Basic seed for Vehicles
+        var vehicleRepository = scope.ServiceProvider.GetRequiredService<IRepository<Vehicle>>();
+        var vehicles = await vehicleRepository.GetAll();
+        if (!vehicles.Any())
+        {
+            var user = await userRepository.Get(1);
+            var vehicleType = await vehicleTypeRepository.Get(1);
+            var newVehicle = new Vehicle
+            {
+                Owner = userRepository.Get(1).Result,
+                RegNumber = "ABC123",
+                Brand = "Volvo",
+                Model = "V70",
+                Color = "Black",
+                NumberOfWheels = 4,
+                VehicleType = vehicleTypeRepository.Get(1).Result,
+            };
+            await vehicleRepository.Add(newVehicle);
+        }
 
         // Basic seed for ParkingEvents
         var parkingEventRepository = scope.ServiceProvider.GetRequiredService<IRepository<ParkingEvent>>();
         var parkingEvents = await parkingEventRepository.GetAll();
         if (!parkingEvents.Any())
         {
-            var user = await userRepository.Get(1);
             var newParkingEvent = new ParkingEvent
             {
                 CheckInTime = DateTime.Now.AddHours(-2),
                 CheckOutTime = DateTime.Now,
-                // waiting for code : Vehicle = 
+                Vehicle = vehicleRepository.Get(1).Result,
             };
             await parkingEventRepository.Add(newParkingEvent);
         }
