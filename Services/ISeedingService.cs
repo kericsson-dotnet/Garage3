bc2873ba636bdd01816e4f25f2b5a5b4d05ade5b
jenscodejens <161465689+jenscodejens@users.noreply.github.com>
2024-05-08 15:58:35 +0200

    Add SeedingService
    ---------
    
    Co-authored-by: Karl Ericsson <lexicon@services.karlericsson.com>

---
+++ Services/ISeedingService.cs
@@ -0,0 +1,13 @@
+﻿using Garage.Models;
+
+namespace Garage.Services
+{
+    public interface ISeedingService
+    {
+        Task AddVehicleTypeSeedsAsync(List<VehicleType> vehicleTypeSeeds);
+        Task AddVehicleSeedsAsync(List<Vehicle> vehicleSeeds);
+        Task AddParkingEventSeedsAsync(List<ParkingEvent> parkingEventSeeds);
+        Task<bool> IsDatabaseSeededAsync();
+        Task SeedDatabaseAsync();
+    }
+}
