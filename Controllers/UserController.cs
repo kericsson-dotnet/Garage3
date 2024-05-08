aeff09bf2557cf0eb58e5239c03a20b4b1dfca4b
Karl Ericsson <lexicon@services.karlericsson.com>
2024-05-08 15:03:43 +0200

    Add park vehicle logic

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,16 +1,18 @@
 using Garage.Models;
 using Garage.Data;
+using Garage.Services;
 using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
 
 namespace Garage.Controllers
 {
     public class UserController : Controller
     {
+        private readonly IGarageService _garageService;
         private readonly IRepository<User> _repository;
 
-        public UserController(IRepository<User> repository)
+        public UserController(IRepository<User> repository, IGarageService garageService)
         {
+            _garageService = garageService;
             _repository = repository;
         }
 
@@ -38,12 +40,37 @@ namespace Garage.Controllers
 
         public async Task<IActionResult> Vehicles(int id)
         {
+            var parkedVehicleIds = new List<int>();
             var user = await _repository.Get(id);
             if (user == null)
             {
                 return NotFound();
             }
+            foreach (var vehicle in user.Vehicles)
+            {
+                if (await _garageService.IsVehicleParkedAsync(vehicle.VehicleId))
+                {
+                    parkedVehicleIds.Add(vehicle.VehicleId);
+                }
+            }
+            ViewBag.ParkedVehicleIds = parkedVehicleIds ?? new List<int>();
             return View(user);
+        }
+        [HttpPost]
+        public async Task<IActionResult> ParkVehicle(int vehicleId)
+        {
+
+            var parkingEvent = await _garageService.ParkVehicleAsync(vehicleId);
+
+            var user = parkingEvent.Vehicle.Owner;
+            return RedirectToAction("Vehicles", new {id = user.UserId});
+        }
+        [HttpPost]
+        public async Task<IActionResult> UnParkVehicle(int vehicleId)
+        {
+            var parkingEvent = await _garageService.UnParkVehicleAsync(vehicleId);
+            var user = parkingEvent.Vehicle.Owner;
+            return RedirectToAction("Vehicles", new {id = user.UserId});
         }
     }
 }
