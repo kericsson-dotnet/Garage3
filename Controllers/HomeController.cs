349c4e141250c579feed3b294c0aebb778a9f2d3
jenscodejens <jensb.code@gmail.com>
2024-05-08 15:50:50 +0200

    Removed duplicate code in the json Seed part. Added: SeedParkingEvents

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,7 @@
-using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
 using Garage.Models;
 using Garage.Services;
-using Garage.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace Garage.Controllers;
 
