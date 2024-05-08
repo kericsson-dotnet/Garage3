240c71142e4254cd6a2fc8433302254b8141f353
fadi abdallah <fadishadow@hotmail.com>
2024-05-08 10:58:19 +0200

    Deleted a prop from User.cs and added som logic to the UserController

--- Models/User.cs
+++ Models/User.cs
@@ -16,9 +16,7 @@ namespace Garage.Models
         public int VehicleId { get; set; }
         public List<Vehicle> Vehicles { get; set; }
 
-        // Property to store the count of vehicles
-        // If Vehicles is null, return 0
-        public int VehicleCount => Vehicles?.Count ?? 0;
+       
         public User()
         {
             Vehicles = new List<Vehicle>();
